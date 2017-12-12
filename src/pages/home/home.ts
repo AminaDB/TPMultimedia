89b7358eb63eb104126786eacda192f5c36ab2aa
Amina Boucherima <amina@wificampus-034246.grenet.fr>
2017-12-12 17:59:50 +0100

    TP3

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -5,6 +5,10 @@ import { Observable } from 'rxjs/Observable';
 import { API_key } from '../../app/tmdb';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { AsyncPipe } from '@angular/common';
+import { AlertController } from 'ionic-angular';
+import { NavController } from 'ionic-angular';
+import { Subscription } from 'rxjs/Subscription';
+import { Shake } from '@ionic-native/shake';
 
 export interface Result {
   title: string;
@@ -21,13 +25,16 @@ export interface Result {
 })
 export class HomePage {
   DetailPage : any = DetailsPage;
+  shakeSubscription : Subscription;
+
 
   films: Observable<Result[]> = Observable.of([]);
-  constructor(private http_client : HttpClient) {
-      
+  
+  constructor(private http_client : HttpClient, public shake : Shake ,public alertCtrl: AlertController, public navCtrl: NavController) {
+    
   }
 
-  getResults($event : any) : void {
+ /* getResults($event : any) : void {
     const val: string = $event.target.value;
     if (val) {
       this.films = this.fetchResults(val);
@@ -35,7 +42,16 @@ export class HomePage {
     else {
       this.films = Observable.of([]);
     }
+  }*/
+
+  ionViewDidLoad() {
+    this.shakeSubscription = this.shake.startWatch() .switchMap(() => this.discoverMovies())
+    .subscribe(movies => this.showRandomMovieAlert(movies));
   }
+  ionViewWillLeave() {
+    this.shakeSubscription.unsubscribe(); 
+  }
+
 
   fetchResults(val:string):Observable<Result[]>{
     return this.http_client.get<Result[]>('https://api.themoviedb.org/3/search/movie',{
@@ -43,5 +59,32 @@ export class HomePage {
     }).pluck('results');
   }
 
+  private discoverMovies():Observable<Result[]>{
+    return this.http_client.get<Result[]>('https://api.themoviedb.org/3/discover/movie',{
+      params: new HttpParams().set('api_key', API_key).set('primary_release_year','2018')
+    }).pluck('results');
+  }
+
+  private showRandomMovieAlert(movies: Result[]): void{
+
+    var movie = movies[Math.floor(Math.random()*movies.length)];
+
+    let confirm = this.alertCtrl.create({
+      title: movie.title,
+      message: movie.overview,
+      buttons: [
+        {
+          text: 'Details',
+          handler: () => {
+            this.navCtrl.push(DetailsPage);
+          }
+        },
+        {
+          text: 'Cancel',
+        }
+      ]
+    });
+    confirm.present();
+  }  
 }
 
